70fa4effab5d55798cc824385d12c50013c7d2de
Jean Tampon <jean.tampon@gmail.com>
2020-12-01 18:08:32 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,17 @@
 
 int main()
 {
+	// The network architecture: 1 input, 3 units in hidden layer, 1 output
 	std::vector<uint32_t> architecture{ 1, 3, 1 };
-
+	// Create the network
 	ffnn::FFNeuralNetwork network(architecture);
+	// Create an optimizer
 	ffnn::Optimizer optimizer(0.01f);
 
+	// Create training set
 	NumberGenerator gen;
-
 	std::vector<ffnn::Vectorf> input_data;
 	std::vector<ffnn::Vectorf> expected_data;
-
 	uint64_t examples_count = 10000;
 	for (uint64_t i(examples_count); i--;) {
 		const float a = gen.getUnder(1.0f);
@@ -22,7 +23,8 @@ int main()
 		expected_data.push_back({ a });
 	}
 
-	const float threshold = 0.6f;
+	// Train the network
+	const float threshold = 1.0f;
 	float error = threshold + 1.0f;
 	while (error > threshold) {
 		error = 0.0f;
@@ -36,12 +38,12 @@ int main()
 		std::cout << error << std::endl;
 	}
 	
-
+	// View results
 	for (uint32_t i(10); i--;) {
 		const float a = gen.getUnder(1.0f);
 		const ffnn::Vectorf r = network.execute({ a });
 		const float error = 0.5f * std::pow(a - r[0], 2);
-		std::cout << "Result for " << a << " " << r[0]  << " error " << error << std::endl;
+		std::cout << "Result for " << a << ": " << r[0]  << " error: " << error << std::endl;
 	}
 
 	return 0;
