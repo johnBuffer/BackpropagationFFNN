d32da92b93b5054d44b31b508b1de961258a3deb
Jean Tampon <jean.tampon@gmail.com>
2024-02-14 23:37:01 +0100

    Reimplementing

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,15 @@
 #include <iostream>
-#include "number_generator.hpp"
-#include "ffnn.hpp"
+#include <Eigen/Dense>
 
-struct Example
-{
-	ffnn::Vectorf input;
-	ffnn::Vectorf output;
-};
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 int main()
 {
-	// The network architecture: 1 input, 3 units in hidden layer, 1 output
-	std::vector<uint32_t> architecture({ 1, 3, 1 });
-	// Create the network
-	ffnn::FFNeuralNetwork network(architecture);
-	// Create an optimizer
-	ffnn::Optimizer optimizer(0.01f);
-
-	// Create training set
-	NumberGenerator gen;
-	std::vector<Example> training_set;
-	uint64_t examples_count = 10000;
-	for (uint64_t i(examples_count); i--;) {
-		const float a = gen.getUnder(1.0f);
-		training_set.push_back({
-			{a},
-			{a}
-		});
-	}
-
-	// Train the network
-	const float threshold = 1.0f;
-	float error = threshold + 1.0f;
-	while (error > threshold) {
-		error = 0.0f;
-		for (const Example& example : training_set) {
-			optimizer.train(network, example.input, example.output);
-			error += optimizer.error;
-		}
-		std::cout << error << std::endl;
-	}
-	
-	// View results
-	for (uint32_t i(10); i--;) {
-		const float a = gen.getUnder(1.0f);
-		const ffnn::Vectorf r = network.execute({ a });
-		const float error = 0.5f * std::pow(a - r[0], 2);
-		std::cout << "Result for " << a << ": " << r[0]  << " error: " << error << std::endl;
-	}
-
-	return 0;
+    MatrixXd m = MatrixXd::Random(3,3);
+    m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    std::cout << "m =" << std::endl << m << std::endl;
+    VectorXd v(3);
+    v << 1, 2, 3;
+    std::cout << "m * v =" << std::endl << m * v << std::endl;
 }
